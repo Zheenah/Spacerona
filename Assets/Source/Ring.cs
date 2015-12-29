574964dae843f082a60e417d06f3539a8276184e
Sina M <sina.martens@gmail.com>
2015-12-29 22:07:26 +0100

    add Ring

---
+++ Assets/Source/Ring.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    public float A;
+    public float B;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
